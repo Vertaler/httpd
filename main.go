0e68631eb9ff010eeb3c7c58f19e4066cd500fde
Vertaler <dancheran@yandex.ru>
2016-09-22 23:47:39 +0300

    Fix command line parsing

--- main.go
+++ main.go
@@ -5,14 +5,18 @@ import (
 	"./tcp_server"
 	"flag"
 	"runtime"
+	"fmt"
 )
 
 func main() {
-	cpunum := *flag.Int("c", runtime.NumCPU(), "-c NCPU")
-	address := *flag.String("a", "localhost:8080", "-a HOST:PORT")
-	root_dir := *flag.String("r", "../../http-test-suite-master", "-r ROOTDIR")
-	log_enable := *flag.Bool("l", false, "-l")
-	factory := handler.CreateFactory(root_dir, log_enable)
-	server := tcp_server.CreateServer(cpunum, address, factory)
+	cpunum := flag.Int("c", runtime.NumCPU(), "-c NCPU")
+	address := flag.String("a", "localhost:8080", "-a HOST:PORT")
+	root_dir := flag.String("r", "../../http-test-suite-master", "-r ROOTDIR")
+	log_enable := flag.Bool("l", false, "-l")
+	flag.Parse()
+	fmt.Println("Num cpu: ", *cpunum)
+	runtime.GOMAXPROCS(*cpunum)
+	factory := handler.CreateFactory(*root_dir, *log_enable)
+	server := tcp_server.CreateServer(*address, factory)
 	server.Start()
 }
--- tcp_server/server.go
+++ tcp_server/server.go
@@ -6,11 +6,8 @@ import (
 	"runtime"
 )
 
-var connection_count int = 0
-var gc_count int = 0
 
 type Server struct {
-	cpu_num int
 	address string
 	factory handler_creator
 }
@@ -23,15 +20,13 @@ type handler_creator interface {
 	CreateHandler(net.Conn) AbstractHandler
 }
 
-func CreateServer(cpunum int, address string, factory handler_creator) *Server {
+func CreateServer(address string, factory handler_creator) *Server {
 	server := Server{}
 	server.address = address
-	server.cpu_num = cpunum
 	server.factory = factory
 	return &server
 }
 func (server *Server) Start() {
-	runtime.GOMAXPROCS(server.cpu_num)
 	fmt.Println("Start server on address " + server.address)
 	listener, err := net.Listen("tcp", server.address)
 	if err != nil {
