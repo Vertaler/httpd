6405d3f31fa8969d47436554218a5dd5f45630da
Vertaler <dancheran@yandex.ru>
2016-09-26 23:18:36 +0300

    Изменён порт по умолчанию

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	cpunum := flag.Int("c", runtime.NumCPU(), "-c NCPU")
-	port := flag.String("p", "8080", "-p PORT")
+	port := flag.String("p", "80", "-p PORT")
 	host := flag.String("h", "localhost", "-h HOST")
 	root_dir := flag.String("r", "../../http-test-suite-master", "-r ROOTDIR")
 	log_enable := flag.Bool("l", false, "-l")
